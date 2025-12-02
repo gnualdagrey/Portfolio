aee41776e6ae135bb16bf9dabc8e1ecd13ef51de
Geoffrey Grey <gnualdagrey@gmail.com>
2025-12-02 14:14:36 +0800

    update base folder

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-3/',
-  css :{
+  base: '/Portfolio/',
+  css: {
     localsConvention: "camelCase "
   }
 })
