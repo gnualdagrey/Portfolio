9e59dd47a0db466ce366809c96d8fdb3cf6358cc
Geoffrey Grey <gnualdagrey@gmail.com>
2025-12-16 14:02:42 +0800

    add a source and fixed the icons error

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -23,7 +23,7 @@ export default function Experience() {
           {
             history.map((historyItem, id) => {
               return <li key={id} className={styles.historyItem}>
-                <img src={getImageUrl(historyItem.imageSrc)} alt={`${historyItem.organisation} Logo`} />
+
                 <div className={styles.historyItemDetails}>
                   <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
