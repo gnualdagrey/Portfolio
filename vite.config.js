732e51a06eb1998f74fc661a0c17e94cca995ed8
Geoffrey Grey <gnualdagrey@gmail.com>
2025-12-16 13:39:35 +0800

    Force rebuild to fix 404 errors on production

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Force rebuild - fixing 404 errors
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
